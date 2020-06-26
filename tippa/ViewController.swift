544f973226a5c4167ed7a4a726fa71088a91a06b
Jacob Haff <jacobhaff2@gmail.com>
2020-06-26 11:19:42 -0400

    Initial Commit

---
+++ tippa/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  tippa
+//
+//  Created by Jacob Haff on 6/26/20.
+//  Copyright © 2020 Jacob Haff. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
