5bc6f69f2359f9fb5bf8be22e5fb53c0f602964c
Jacob Haff <jacobhaff2@gmail.com>
2020-06-26 12:48:10 -0400

    Scaffold CalculatorViewController

---
+++ tippa/CalcuatorViewController.swift
@@ -0,0 +1,31 @@
+//
+//  ViewController.swift
+//  tippa
+//
+//  Created by Jacob Haff on 6/26/20.
+//  Copyright © 2020 Jacob Haff. All rights reserved.
+//
+
+import UIKit
+
+class CalcuatorViewController: UIViewController {
+
+    @IBOutlet weak var billTextField: UITextField!
+    @IBOutlet weak var zeroPctButton: UIButton!
+    @IBOutlet weak var tenPctButton: UIButton!
+    @IBOutlet weak var twentyPctButton: UIButton!
+    @IBOutlet weak var splitNumberLabel: UILabel!
+    
+    @IBAction func tipChanged(_ sender: UIButton) {
+        
+    }
+    
+    @IBAction func stepperValueChanged(_ sender: UIStepper) {
+        
+    }
+    	
+    @IBAction func calculatePressed(_ sender: UIButton) {
+        
+    }
+}
+
